f009e4649805feea39ffaf7d201ffe10573895f0
1F10220260 <s1f102202606@iniad.org>
2023-07-06 11:23:44 +0900

    コードを整形1

--- todo/tests.py
+++ todo/tests.py
@@ -3,10 +3,12 @@ from django.utils import timezone
 from datetime import datetime
 from todo.models import Task
 
+
 # Create your tests here.
 class SampleTestCase(TestCase):
     def test_sample1(self):
         self.assertEqual(1 + 2, 3)
+
 
 class TaskModelTestcase(TestCase):
     def test_create_task1(self):
@@ -51,6 +53,7 @@ class TaskModelTestcase(TestCase):
 
         self.assertFalse(task.is_overdue(current))
 
+
 class TodoViewTestCase(TestCase):
     def test_index_get(self):
         client = Client()
@@ -63,7 +66,7 @@ class TodoViewTestCase(TestCase):
     def test_index_post(self):
         client = Client()
         data = {"title": "Test Task", "due_at": "2023-06-30 23:59:59"}
-        response = client.post("/",data)
+        response = client.post("/", data)
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.templates[0].name, "todo/index.html")
